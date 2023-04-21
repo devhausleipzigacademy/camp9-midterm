49ccf4a67d91c4808445a78b45126b5ca0ac1ea8
Nnek Nnek <hillmar@posteo.de>
2023-04-21 16:09:03 +0200

    wrote function to randomly choose dates, disable those and sort them so that 12 dates are displayed in the right order

--- src/components/BookingDate.tsx
+++ src/components/BookingDate.tsx
@@ -1,10 +1,11 @@
+import { Children } from 'react';
 import BookingBtn from './BookingBtn';
 
 export default function BookingDate() {
   const currentDate = new Date();
+  //initialising a new Date, 'new' means accessing a fct (otherwise it could be confused with a type)
   const end = new Date();
   const movieDurationInMinutes = 90;
-
   end.setHours(23, 0, 0, 0);
 
   const monthNames = [
@@ -23,6 +24,8 @@ export default function BookingDate() {
   ];
 
   function addDays(date: Date, days: number) {
+    //build-in types exist! like array, number...
+    //type 'Date" has a specific format:
     const newDate = new Date(date);
     newDate.setDate(newDate.getDate() + days);
     const day = newDate.getDate();
@@ -32,26 +35,49 @@ export default function BookingDate() {
   }
 
   const dateBtns = [];
-
+  // make array with current dates
   for (let i = 0; i < 12; i++) {
     const label = addDays(currentDate, i);
+    //label could also be just children
     const bookingBtn = <BookingBtn key={i} children={label} />;
     dateBtns.push(bookingBtn);
   }
 
-  const timeBtns = [];
+  // then select 4 out of dateBtns
+  // const days in manuels new code
+  function FourDates(dateBtns: JSX.Element[]) {
+    const disabledDates: JSX.Element[] = [];
+    while (disabledDates.length < 4) {
+      const randomIndex = Math.floor(Math.random() * dateBtns.length);
+      if (!disabledDates.includes(dateBtns[randomIndex])) {
+        disabledDates.push(dateBtns[randomIndex]);
+      } //splice returns modified array
+      dateBtns.splice(randomIndex, 1);
+    }
 
-  for (let i = 0; currentDate < end; i++) {
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const roundedMinutes =
-      Math.floor(minutes / movieDurationInMinutes) * movieDurationInMinutes;
-    const label = `${hours.toString().padStart(2, '0')}:${roundedMinutes
-      .toString()
-      .padStart(2, '0')}`;
-    const bookingBtn = <BookingBtn key={i} children={label} />;
-    timeBtns.push(bookingBtn);
-    currentDate.setMinutes(currentDate.getMinutes() + movieDurationInMinutes);
+    const disabledDatesWithId = disabledDates.map(item => {
+      return (
+        <BookingBtn key={item.key} children={item.props.children} disabled />
+      );
+    });
+    return disabledDatesWithId;
   }
-  return <>{dateBtns}</>;
+
+  const finalDisabledDates = FourDates(dateBtns);
+
+  //combine both arrays (8+4) into one (12)
+  const allDates = [...dateBtns, ...finalDisabledDates];
+  console.log(allDates.keys);
+
+  return (
+    <>
+      {/* sort dates by key */}
+      {allDates.sort(function (a, b) {
+        let x = parseInt(a.key as string);
+        let y = parseInt(b.key as string);
+        const result = x - y;
+        return result;
+      })}
+    </>
+  );
 }
