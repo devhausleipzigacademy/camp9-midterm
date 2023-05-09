4eb369a31cfbfc4daa0b700fc4393c10930b00eb
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-05-09 10:13:04 +0200

    EditProfileForm sshow the values from the back end

--- frontend/src/hooks/useGetSingleUser.ts
+++ frontend/src/hooks/useGetSingleUser.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 import { User } from '../utilities/types';
 
 const getSingleUser = async (id: string) => {
-  const { data } = await axios.get<User>(`/api/1.0/user/${id}`);
+  const { data } = await axios.get<User>(
+    `${import.meta.env.VITE_BACKEND_URL}/api/1.0/user/${id}`
+  );
   return data;
 };
 
--- frontend/src/pages/EditProfileForm.tsx
+++ frontend/src/pages/EditProfileForm.tsx
@@ -2,17 +2,26 @@ import Button from '../components/Button';
 import { useGetSingleUser } from '../hooks/useGetSingleUser';
 
 function EditProfileInputForm() {
+  // fetch user data using axios from backend
   const user = useGetSingleUser('048338d5-5cb9-4e5d-8129-aaf8d5d33395');
-  console.log(user);
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log('submit');
+  }
+
   return (
-    <form className="flex flex-col gap-36 p-4 h-full content-between justify-between">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-36 p-4 h-full content-between justify-between"
+    >
       <div className="flex flex-col gap-5 p-4">
         <div>
           <label
             htmlFor="Change first name"
             className="text-white dark:text-dark text-lg"
           >
-            Name
+            {user?.data?.firstName}
           </label>
           <input
             type="text"
@@ -27,13 +36,13 @@ function EditProfileInputForm() {
             htmlFor="Change last name"
             className="text-white text-lg dark:text-dark"
           >
-            Lastname
+            {user?.data?.lastName}
           </label>
           <input
             type="text"
             name="Change last name"
             id="Change last name"
-            placeholder="Change your lastname"
+            placeholder="Change your last name"
             className="w-full rounded-lg bg-dark-light h-[48px] flex items-center justify-between p-5 gap-5 dark:text-white"
           />
         </div>
@@ -42,7 +51,7 @@ function EditProfileInputForm() {
             htmlFor="Change email"
             className="text-white text-lg dark:text-dark"
           >
-            Email
+            {user?.data?.email}
           </label>
           <input
             type="email"
