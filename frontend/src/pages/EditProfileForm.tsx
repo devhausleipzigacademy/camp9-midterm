3ab79a9af992604778d0445fd180cc53ea3b94cc
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-05-12 15:28:45 +0200

    fixed stayling for Account settings page

--- frontend/src/pages/EditProfileForm.tsx
+++ frontend/src/pages/EditProfileForm.tsx
@@ -2,12 +2,12 @@ import Button from '../components/Button';
 
 function EditProfileInputForm() {
   return (
-    <form className="flex flex-col gap-36 p-4 h-full content-between justify-between">
+    <form className="flex flex-col gap-36 px-4 h-full content-between justify-between">
       <div className="flex flex-col gap-5 p-4">
         <div>
           <label
             htmlFor="Change first name"
-            className="text-white dark:text-dark text-lg"
+            className="text-white dark:text-dark"
           >
             Name
           </label>
@@ -15,42 +15,40 @@ function EditProfileInputForm() {
             type="text"
             name="Change first name"
             id="Change first name"
-            placeholder="Change your name"
             className="w-full rounded-lg bg-dark-light h-[48px] flex items-center justify-between p-5 gap-5 dark:text-white"
           />
         </div>
         <div>
           <label
             htmlFor="Change last name"
-            className="text-white text-lg dark:text-dark"
+            className="text-white dark:text-dark"
           >
-            Lastname
+            Last name
           </label>
           <input
             type="text"
             name="Change last name"
             id="Change last name"
-            placeholder="Change your lastname"
             className="w-full rounded-lg bg-dark-light h-[48px] flex items-center justify-between p-5 gap-5 dark:text-white"
           />
         </div>
         <div>
-          <label
-            htmlFor="Change email"
-            className="text-white text-lg dark:text-dark"
-          >
+          <label htmlFor="Change email" className="text-white  dark:text-dark">
             Email
           </label>
           <input
             type="email"
             name="Change email"
             id="Change email"
-            placeholder="Change your email"
             className="w-full rounded-lg bg-dark-light h-[48px] flex items-center justify-between p-5 gap-5 dark:text-white"
           />
         </div>
       </div>
-      <Button size="md">Save Changes</Button>
+      <div className="p-8 mt-auto w-full">
+        <Button >
+          Save Changes
+        </Button>
+      </div>
     </form>
   );
 }
